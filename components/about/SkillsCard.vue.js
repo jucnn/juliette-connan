aaa6cb67040c5ff802c1864f286954e5df1d679a
Juliette <juliette.connan@vmed.fr>
2021-02-14 00:27:20 +0100

    works card

---
+++ components/about/SkillsCard.vue.js
@@ -0,0 +1,7 @@
+Vue.component("skills-card", {
+    template: `
+    <div class="skills-card">
+        <h3>Skills card</h3>
+    </div>
+    `,
+});
--- components/works/WorksCard.vue.js
+++ components/works/WorksCard.vue.js
@@ -1,8 +1,13 @@
 Vue.component("works-card", {
-    template: `
-    <div>
-        <h2>Works card</h2>
-        <p id="highlight">Stellar Interactiv</p>   
+  template: `
+    <div class="works-card">
+        <img class="works-card_img" :src="image" />
+        <div class="works-card_description">
+            <p class="works-card_category"><img src="./assets/img/icons/detail-wave-yellow.svg"/>{{category}}</p>
+            <p class="highlight">{{title}}</p>
+            <a :href="url">learn more</a>   
+        </div>
     </div>
     `,
+  props: ["title", "category", "url", "image"],
 });
